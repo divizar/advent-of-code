70eabd2a59fecc3be70e387daa11a6e1ede12aed
divizar <joshidivij9@gmail.com>
2025-12-01 18:15:09 +0530

    Add day 1 folder with template

---
+++ 2025/day1/README.md
@@ -0,0 +1,8 @@
+# Day 01 — Problem Name
+
+## Part 1
+TODO
+
+## Part 2
+TODO
+
---
+++ 2025/day1/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<string> lines;
+    string s;
+    while (getline(cin, s)) {
+        lines.push_back(s);
+    }
+
+    // Part 1
+    // TODO
+
+    // Part 2
+    // TODO
+
+    return 0;
+}
+
